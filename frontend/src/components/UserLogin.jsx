f036e17fce6d550f7bbc34473e05ad9ac4571a1d
Marcos Lorhan <marcoslorhanbs@gmail.com>
2023-07-13 14:39:52 -0300

    Responsivity Update

--- frontend/src/components/UserLogin.jsx
+++ frontend/src/components/UserLogin.jsx
@@ -4,7 +4,8 @@ import { Form, Button } from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
 import App from '../App';
 import SignUp from './UserRegister';
-
+import ifpb from '../img/IFCZ.png'
+import ifBrand from '../img/IFBrand.png'
 
 
 
@@ -55,6 +56,7 @@ function SignIn() {
       {showApp && <App />}
       {showRegister && <SignUp />}
       {showLogin && 
+        <div>
         <Container className='bg-light rounded'>
           <Form onSubmit={enviarRequisicao} method='POST' action='http://127.0.0.1:8000/api/auth-user/'>
           <Form.Group className="mb-3" controlId="formBasicUsername">
@@ -78,6 +80,8 @@ function SignIn() {
         </Form>
         <p>Não Possui uma Conta? <span onClick={handleRegister} className='btn btn-primary btn-sm'>Clique Aqui</span> </p>
       </Container>
+        </div>
+       
       }
       
     </>
